357679d6e4cbc53f917f3cb684799c12aa5448e3
AnaaCrakinha <ana954664@gmail.com>
2025-02-11 10:49:59 -0300

    aula1

---
+++ Aula 1- 11/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const mela = express();
+
+const port = 3000;
+
+mela.get("/", (req, res) => {
+res.send("Olá, como você está? este servidor está rodanso com express");
+});
+
+mela.get("/home", (req, res) => {
+    res.send("Olá, como você está? este servidor está rodanso com express");
+    });
+
+
+    mela.get("/login", (req, res) => {
+        res.send("Olá, como você está? este servidor está rodanso com express");
+        });
+
+
+mela.listen(port, () => { 
+    console.log("Servidor rodando na porta", port);
+});
